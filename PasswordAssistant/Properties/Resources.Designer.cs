08a56f39d96f7e718944544be2e29cf6dd2d38d8
haskil <haskil37@gmail.com>
2017-01-19 15:00:33 +0300

    add ico

--- PasswordAssistant/Properties/Resources.Designer.cs
+++ PasswordAssistant/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace PasswordAssistant.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon _lock1 {
+            get {
+                object obj = ResourceManager.GetObject("_lock1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
